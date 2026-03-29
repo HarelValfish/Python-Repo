ab348f8c1b19cf132e78fdfa66f6d6d482a482e3
HarelValfish <harelvalfish@gmail.com>
2026-03-29 10:28:11 +0300

    Loops basics file added, Renamed new app to new-appli

---
+++ loops/for_loops.py
@@ -0,0 +1,12 @@
+# for loops basics
+
+#===================
+
+# friends = ["Rolf", "Jen", "Bob", "Anne"]
+# for i in friends:
+#     print(f"{i} is my friend.")
+
+#==========================================
+
+# for num in range(2, 10, 3): # 2 through 9
+#     print(f"{num} is my friend.")
