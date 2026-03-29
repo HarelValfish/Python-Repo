c620b5de32c7bafb727758aab79e173f6ccc887f
HarelValfish <harelvalfish@gmail.com>
2026-03-29 10:07:40 +0300

    While loops basics file added

---
+++ loops/while_loops.py
@@ -0,0 +1,17 @@
+# While loops basics
+
+#=====================
+
+# user_input = input("Would you like to play? (y/n): ")
+# while user_input != "n":
+#     print("Game is running. . .")
+#     user_input = input("Would you like to play again? (y/n): ")
+    
+#================================================================
+    
+# while True:
+#     user_input = input("Would you like to play? (y/n): ")
+    
+#     if user_input == "n":
+#         break
+#     print("Game is running...")
