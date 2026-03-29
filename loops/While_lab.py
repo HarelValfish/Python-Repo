35baa8fd43ed7a6736e177df4237c4b290fddd18
HarelValfish <harelvalfish@gmail.com>
2026-03-29 11:46:19 +0300

    for and while labs added to loops directory

---
+++ loops/While_lab.py
@@ -0,0 +1,67 @@
+# #1
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+    
+# #2
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1 
+
+# #3
+
+# i = 2
+# while i <= 10:
+#     print(i)
+#     i += 2
+    
+# #4
+
+# i = 1
+# sum = 0
+# while i <= 5:
+#     sum = sum + i
+#     i = i + 1
+# print("The sum is:", sum)
+
+# #5
+
+# i = 3
+# while i < 10:
+#     print(i)
+#     i = i + 3
+    
+# #6
+
+# user_inp = int(input("Enter a number: "))
+# i = 1
+# while i <= 5:
+#     print(user_inp)
+#     i = i + 1
+    
+# #7
+
+# user_inp = int(input("Enter a number: "))
+# i = 1
+# while i <= user_inp:
+#     print(i)
+#     i += 1
+    
+# #8
+
+# num = -1
+# while num != 0:
+#     num = int(input("Enter number: "))
+# print("Stopped")
+
+# #9
+
+# i = -1
+# pswrd = 1234
+# while i != pswrd:
+#     i = user_input = int(input("Enter password: "))
+# print("correct")
---
+++ loops/for_lab.py
@@ -0,0 +1,76 @@
+# #1
+
+# for n in range(1, 6):
+#     print(n)
+
+# #2
+
+# for i in range(5, 0, -1):
+#     print(i)
+    
+# #3
+
+# for i in range(2, 11, 2):
+#     print(i)
+    
+# #4
+
+# for i in range(1, 11):
+#     print(i)
+
+# #5
+
+# for i in range(3, 21, 3):
+#     print(i)
+
+# #6
+
+# for i in range(5):
+#     print("Hello")
+
+# #7
+
+# for i in range(10):
+#     print(i)
+
+# #8
+
+# for i in range(2, 21, 2):
+#     print(i)
+
+# #9
+
+# for i in range(11):
+#     print(i)
+
+# #10
+
+# total = 0
+# for i in range(1, 101):
+#     total = i + total
+# print(total)
+
+# #11,12
+
+# i = 0
+# for n in ["bob", "jen", "mike"]:
+#     print(n)
+#     i += 1
+# print(i)
+
+# #13
+
+# tot = 0
+# for n in [1, 5, 8, 10]:
+#     tot = n + tot
+# print(tot)
+
+# #14
+
+# nums = [12,53,75,2,54]
+# max = nums[0]
+# for num in nums:
+#     if num > max:
+#         max = num
+# print("The largest number is: ", max)
+
