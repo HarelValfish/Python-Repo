c3862e7bbcedcadc1a1b8662d79855e5d5a579fb
HarelValfish <harelvalfish@gmail.com>
2026-03-29 10:57:48 +0300

    For loops file updated with more content

--- loops/for_loops.py
+++ loops/for_loops.py
@@ -10,3 +10,25 @@
 
 # for num in range(2, 10, 3): # 2 through 9
 #     print(f"{num} is my friend.")
+
+#==========================================
+
+# grades = [80, 75, 90, 100, 85]
+# total = 0
+# amount = len(grades)
+
+# for n in grades:
+#     total = total + n
+# avg = total / amount
+# print(f"Amount of grades: {amount}")
+# print(f"Average grade is {avg}")
+
+#==========================================
+
+# grades = [80, 75, 90, 100, 85]
+# total = sum(grades)
+# amount = len(grades)
+
+# avg = total / amount
+# print(f"Amount of grades: {amount}")
+# print(f"Average grade is {avg}")
