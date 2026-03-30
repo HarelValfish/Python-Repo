96ede8afd2278260b2c6e711aa6c4f5c2a809533
HarelValfish <harelvalfish@gmail.com>
2026-03-30 12:05:25 +0300

    Bug fixes

--- ATM-App/main2.py
+++ ATM-App/main2.py
@@ -1,79 +1,78 @@
 accounts = [{
     "account_id": 1001,
-    "username": "Harel",
-    "balance": 50000,
-    "pin": 4331
+    "username": "Yuval",
+    "balance": 55000,
+    "pin": 3571
 },
 {
     "account_id": 1002,
     "username": "Mike",
-    "balance": 1000000,
+    "balance": 100000,
     "pin": 1234
 }]
 
 def auth():
-    id = int(input("Enter ID:"))
-    pin = int(input("Enter PIN: "))
+    id = int(input("Enter your account id"))
+    pin = int(input("enter your PIN: "))
     
+    # Find matching account from id
     for user in accounts:
         if id == user["account_id"] and user["pin"] == pin:
             print(f"Welcome {user["username"]}")
-            return user
-        else:
-            exit()
+            return user # exists => positive => True => 1
+    
+    return None # non-exisits => negative => False => 0
 
 def withdraw():
     user_account = auth()
     if user_account:
-        amount = int(input("Please type withdrawal amount: "))
+        amount = float(input("Please type the amount you would like to withdraw: "))
         if amount > user_account["balance"]:
-            print("insufficient amount.")
-            exit()
+            print("You are poor")
         else:
             user_account["balance"] -= amount
             balance(user_account["balance"])
     else:
-        print("Wrong pin, goodbye")
+        print("Wrong PIN, goodbye")
         exit()
 
-def deposit():
+def deposite():
     user_account = auth()
     if user_account:
-        amount = float(input("Please type deposit amount: "))
+        amount = float(input("Please type the amount you would like to deposite: "))
         if amount < 0:
-            print("Invalid amount.")
+            print("You cannot tpye negative numbers")
             exit()
         else:
             user_account["balance"] += amount
             balance(user_account["balance"])
-            
     else:
-        print("Wrong pin, Goodbye")
+        print("Wrong PIN, goodbye")
         exit()
-    
-def balance(user_balance):
-    print(f"Your balance is {user_balance}")
-    
-while True:
-    print("========= Welcome to Python ATM =========")
-    x = int(input("""
-          Enter your choice
-          1. Withdraw
-          2. Deposit
-          3. Balance
-          0. Exit
-          User Choice: """))
 
+def balance(user_balance):
+    print(f"Your Balance now is {user_balance}")
+
+
+while True:
+    print("======= Welcome to Python ATM =======")    
+    user_choise = int(input("""
+        Choose one: 
+        1. withdraw Money
+        2. Deposite Money
+        3. Balance
+        0. to exit
+    """))
     
-    match x:
+    # print(f"User Choosed {user_choise}")
+    
+    match user_choise:
         case 1:
             withdraw()
         case 2:
-            deposit()
+            deposite()
         case 3:
             balance()
         case 0:
-            print("")
+            print("Ok, Exiting...")
             exit()
-
-        
